e5118732efe04e3373775a1ef26eb61f888e01cd
Filip Stefaniak <stefaniak@gmail.com>
2026-02-11 15:33:59 +0100

    precise requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="optuclust",
-    version="0.0.1",
+    version="0.0.2",
     author="Filip S.",
     author_email="filip.ursynow@gmail.com",
     description="Hyperparameter optimization for multiple clustering algorithms using Optuna, with Scikit-learn API",
